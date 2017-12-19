609e59ffb293e2c318a73688cd917e30b4888e3c
rich1081 <rich1081@yahoo.com>
2017-12-19 13:40:04 -0600

    set up files

---
+++ server.js
@@ -0,0 +1,9 @@
+//Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+var PORT = 3000;
+
+app.use(bodyParser.urlencoded())
