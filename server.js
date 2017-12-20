041077a9232df77f6938aee90ae6f65c148ddf00
rich1081 <rich1081@yahoo.com>
2017-12-20 15:38:01 -0600

    updating file folders

--- server.js
+++ server.js
@@ -6,4 +6,18 @@ var path = require("path");
 var app = express();
 var PORT = 3000;
 
-app.use(bodyParser.urlencoded())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(express.static("app/public/assets/css"));
+
+require('./app/routing/htmlRoutes.js')(app);
+// require('./app/routing/apiRoutes.js')(app, bodyParser, path);
+
+
+var PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function() {
+	console.log("listening on " + PORT);
+});
